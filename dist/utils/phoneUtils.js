27fd2652d572624a28ccaf5dd31c84a416180a93
chahatgoyal <chahatgoyal1999@gmail.com>
2025-06-30 23:52:06 +0530

    chore: remove dist folder from repo and add to .gitignore

--- dist/utils/phoneUtils.js
+++
@@ -1,5 +0,0 @@
-export function formatPhoneNumber(phoneNumber) {
-    // Remove 'whatsapp:' prefix and trim spaces
-    return phoneNumber.replace('whatsapp:', '').replace(/\s+/g, '');
-}
-// Remove generateJoinLink, as it was only used for sandbox onboarding 
